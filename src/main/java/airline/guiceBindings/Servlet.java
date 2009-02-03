d0bb7b61847ee29728fbfff26b459e351f220775
David Duponchel <dev@sora.(none)>
2009-02-03 20:47:45 +0100

    working start of mvc with jsp

---
+++ src/main/java/airline/guiceBindings/Servlet.java
@@ -0,0 +1,21 @@
+/*
+ * Created by IntelliJ IDEA.
+ * User: dev
+ * Date: 3 févr. 2009
+ * Time: 20:04:31
+ */
+package airline.guiceBindings;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Scopes;
+import airline.dao.AirlineDAO;
+import airline.dao.impl.AirlineDAOImpl;
+import airline.connector.Connector;
+import airline.connector.impl.ConnectorTestImpl;
+
+public class Servlet extends AbstractModule {
+    protected void configure() {
+        bind(Connector.class).to(ConnectorTestImpl.class).in(Scopes.SINGLETON);
+        bind(AirlineDAO.class).to(AirlineDAOImpl.class).in(Scopes.SINGLETON);
+    }
+}
--- src/main/java/airline/servlet/Accueil.java
+++ src/main/java/airline/servlet/Accueil.java
@@ -1,5 +1,9 @@
 package airline.servlet;
 
+import com.google.inject.Inject;
+import com.google.inject.Injector;
+import com.google.inject.Guice;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -7,17 +11,37 @@ import javax.servlet.ServletException;
 import javax.servlet.RequestDispatcher;
 import java.io.IOException;
 
+import airline.dao.AirlineDAO;
+import airline.guiceBindings.Servlet;
+
 /**
  * Servlet accueil
  */
 public class Accueil extends HttpServlet {
+    private AirlineDAO airlineDAO;
+
+    public Accueil() {
+
+        Injector injector = Guice.createInjector(new Servlet());
+        injector.injectMembers(this);
+    }
+
+    @Inject
+    public void setAirlineDAO(AirlineDAO airlineDAO) {
+        this.airlineDAO = airlineDAO;
+    }
+
+
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        RequestDispatcher dispatcher = this.getServletContext().getRequestDispatcher("Response.jsp");			
+        request.setAttribute("graou", airlineDAO.getTables());
+        
+        RequestDispatcher dispatcher = this.getServletContext().getRequestDispatcher("/accueil.jsp");
         dispatcher.forward(request,response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        RequestDispatcher dispatcher = this.getServletContext().getRequestDispatcher("/accueil.jsp");
+        dispatcher.forward(request,response);
     }
 }
