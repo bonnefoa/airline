a1150ce8ed538df6e3a1b475e02f7d981f909821
sora <sora@shiki.(none)>
2009-02-10 22:48:01 +0100

    Correction on code

--- xlsParser/src/main/java/xlsParser/App.java
+++ xlsParser/src/main/java/xlsParser/App.java
@@ -26,7 +26,6 @@ import java.sql.SQLException;
  * Main method
  */
 public class App {
-    private static Handler handler;
 
     public static void main(String[] args) throws BiffException, IOException, SQLException {
         if (args.length < 2) {
@@ -34,7 +33,7 @@ public class App {
             System.out.println("[Script de creation sql de la base] [Donnees sous format xsl] ");
         } else {
             System.out.println("Parse :");
-            handler = new Handler(args[0], args[1]);
+            Handler handler = new Handler(args[0], args[1]);
             handler.execute();
             System.out.println("Parse finit");
         }
