21775209c65bd4b1c745cc4f49660c534e1fa1a8
sora <sora@shiki.(none)>
2009-02-11 16:27:26 +0100

    Externalisation du login/mdp admin

--- src/main/java/airline/dao/impl/AuthDAOImpl.java
+++ src/main/java/airline/dao/impl/AuthDAOImpl.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (C) 2009 Anthonin Bonnefoy and David Duponchel
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *         http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,6 +19,9 @@ package airline.dao.impl;
 import airline.dao.AuthDAO;
 import airline.model.User;
 
+import java.util.ResourceBundle;
+import java.util.MissingResourceException;
+
 /**
  * Created by IntelliJ IDEA.
  * User: dev
@@ -27,6 +30,9 @@ import airline.model.User;
  * To change this template use File | Settings | File Templates.
  */
 public class AuthDAOImpl implements AuthDAO {
+
+    private ResourceBundle resourceBundle = ResourceBundle.getBundle(AuthDAO.class.getCanonicalName());
+
     public boolean isLoggedIn(User user) {
         return (user != null);
     }
@@ -36,7 +42,17 @@ public class AuthDAOImpl implements AuthDAO {
             return false;
         }
 
-        if ("admin".equals(user.getLogin()) && "adminadmin".equals(user.getPasswd())) {
+        String login;
+        String password;
+        try {
+            login = resourceBundle.getString("login");
+            password = resourceBundle.getString("password");
+        } catch (MissingResourceException e) {
+            login = "admin";
+            password = "adminadmin";
+        }
+
+        if (login.equals(user.getLogin()) && password.equals(user.getPasswd())) {
             user.setLogged(true);
             return true;
         } else {
